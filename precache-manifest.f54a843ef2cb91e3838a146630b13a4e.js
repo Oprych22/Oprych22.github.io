e282133246bf7368633c6036701aed9cafefc86b
Michael <oprych22@gmail.com>
2020-10-08 21:18:30 +0100

    Updates

---
+++ precache-manifest.f54a843ef2cb91e3838a146630b13a4e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "63ca79cd2f3140b32ffacef7ecf6276d",
+    "url": "/Oprych22/Oprych22.github.io/index.html"
+  },
+  {
+    "revision": "ef680023c2d0133b1d45",
+    "url": "/Oprych22/Oprych22.github.io/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "3005a3d08198d63e4758",
+    "url": "/Oprych22/Oprych22.github.io/static/js/2.6931f07e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Oprych22/Oprych22.github.io/static/js/2.6931f07e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ef680023c2d0133b1d45",
+    "url": "/Oprych22/Oprych22.github.io/static/js/main.118a3617.chunk.js"
+  },
+  {
+    "revision": "28b12453c3315c337014",
+    "url": "/Oprych22/Oprych22.github.io/static/js/runtime-main.7be39c6a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/Oprych22/Oprych22.github.io/static/media/logo.5d5d9eef.svg"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/Oprych22/Oprych22.github.io/precache-manifest.f54a843ef2cb91e3838a146630b13a4e.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Oprych22/Oprych22.github.io/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
